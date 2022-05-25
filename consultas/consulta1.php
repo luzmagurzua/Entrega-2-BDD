bf7e387748c36915e5b4fa71445ef5ac0e442764
LUZ MARIA GARCIA <luzmagurzua@uc.cl>
2022-05-25 09:41:14 -0400

    Problemas con la Consulta 1

--- consultas/consulta1.php
+++ consultas/consulta1.php
@@ -1,3 +1,32 @@
-<!DOCTYPE html>
+<?php include('../templates/header.html');   ?>
 
-<h1> holaa consulta 1</h1>
+<body>
+
+<?php
+  #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+  require("../config/conexion.php");
+
+ 	$query = "SELECT DISTINCT codigo FROM fpl_y_propuestas WHERE estado = 'pendiente';";
+
+	$result = $db -> prepare($query);
+	$result -> execute();
+	$vuelos_pendientes = $result -> fetchAll();
+
+	echo $vuelos_pendientes
+	
+?>
+
+<table>
+    <tr>
+      <th>ID</th>
+      <th>Nombrehdfghdjfghdbghfdhjf</th>
+      <th>Tipo</th>
+    </tr>
+  <?php
+	foreach ($vuelos_pendientes as $vuelo) {
+  		echo "<tr> <td>$vuelo</td> <td></td> <td></td> </tr>";
+	}
+  ?>
+	</table>
+
+<?php include('../templates/footer.html'); ?>
